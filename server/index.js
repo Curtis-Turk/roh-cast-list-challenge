8f6e96a55264b908ff9f741b425834adc45460fc
Curtis Turk <curtis.c.turk@outlook.com>
2023-01-27 12:20:12 +0000

    Fetching data from RoH

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import pkg from "body-parser";
 const { json } = pkg;
+import fetch from "node-fetch";
 
 const app = express();
 app.use(cors());
@@ -10,6 +11,14 @@ const port = 3001;
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello World!" });
+});
+
+app.get("/Turandot", async (req, res) => {
+  const turandotUrl =
+    "https://www.roh.org.uk/api/event-details?slug=turandot-by-andrei-serban";
+  const response = await fetch(turandotUrl);
+  const data = await response.json();
+  res.json({ data: data });
 });
 
 app.listen(port, () => {
