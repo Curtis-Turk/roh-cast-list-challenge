53b1e7373d16e57e98d5c5d7f3a60a2901714014
Curtis Turk <curtis.c.turk@outlook.com>
2023-01-27 13:05:01 +0000

    Getting creatives

--- client/src/components/Performance.jsx
+++ client/src/components/Performance.jsx
@@ -5,6 +5,15 @@ import fetchEventDetails from "../api/fetchEventDetails";
 function Performance({ production, date }) {
   const [performanceData, setPerformanceData] = useState({});
 
+  const getCreatives = (data) => {
+    const creatives = [];
+    data.included.forEach((relationship) => {
+      if (relationship.type === "creatives") {
+        creatives.push(relationship.attributes.name);
+      }
+    });
+    return creatives;
+  };
   useEffect(() => {
     fetchEventDetails(production)
       .then((data) => {
@@ -12,6 +21,7 @@ function Performance({ production, date }) {
         setPerformanceData({
           title: productionData.attributes.title,
           shortDescription: productionData.attributes.shortDescription,
+          creatives: getCreatives(data),
         });
         console.log(data);
       })
@@ -23,6 +33,8 @@ function Performance({ production, date }) {
       <h1>{performanceData.title}</h1>
       <p>Date: {date}</p>
       <h3>{performanceData.shortDescription}</h3>
+      <h2>Creatives</h2>
+      <ul>{performanceData.creatives}</ul>
     </div>
   );
 }
